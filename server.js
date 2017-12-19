e24e8e40b1af6dacaa7199208bba3ee793a595e1
Gareth <gsveitch@gmail.com>
2017-12-18 23:22:21 -0600

    rebuild app.js; tweak procfile / env for port; create client folder. woohoo

---
+++ server.js
@@ -0,0 +1,29 @@
+// APP REQUIREMENTS
+require('dotenv').config();
+const express = require('express');
+const app = express();
+var PORT = process.env.PORT;
+
+// MONGOOSE REQUIREMENTS
+const { MONGO_URI } = process.env;
+const mongoose = require('mongoose');
+mongoose.connect(MONGO_URI, {useMongoClient: true});
+
+//MONGOOSE SETUP
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log(`connected to mongoose at ${MONGO_URI}`)
+});
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+
+//NOW LISTEN
+app.listen(PORT, (err) => {
+    console.log(err || `listening on port ${PORT}`)
+});
+
