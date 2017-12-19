e99f1bac49b2b349e4330726b4639dc52b6920e2
Gareth <gsveitch@gmail.com>
2017-12-19 16:32:00 -0600

    three way conditional rendering and button clicks baby!

--- client/components/app.js
+++ client/components/app.js
@@ -3,7 +3,27 @@ angular.module('my-top-5')
 .component('app', {
   templateUrl: '/templates/app.html',
   controller: function() {
-    this.top5s = window.exampleVideoData;
-    this.home = true;
+    this.top5s = window.fakeTop5s;
+    this.currentTop5 = window.fakeTop5s[0];
+    this.whereAmI = 'home';
+    //something to store top 5 while being built, before pushing to database
+    //perhaps a constructor function???
+    this.top5bucket = {}; 
+
+    this.displayHome = () => {
+      this.whereAmI = 'home';
+      console.log(`hey! i'm at ${this.whereAmI}`)
+    };
+    this.displayMaker = () => {
+      this.whereAmI = 'maker';
+      console.log(`hey! i'm at ${this.whereAmI}`)
+    }
+    this.displayTop5 = (top5) => {
+      this.whereAmI = 'top5';
+      console.log(`hey! i'm at ${this.whereAmI}`)
+    }
+    this.displayHome = this.displayHome.bind(this);
+    this.displayMaker = this.displayMaker.bind(this);
+    this.displayTop5 = this.displayTop5.bind(this);
   }
 });
