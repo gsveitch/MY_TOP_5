9e45fa1ccc62adffee56c9009f7f2bcfd4f1d9a8
Gareth <gsveitch@gmail.com>
2017-12-20 07:39:12 -0600

    tried to add database functionality on the fly

--- server.js
+++ server.js
@@ -4,26 +4,45 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT;
-
-// MONGOOSE REQUIREMENTS
-const { MONGO_URI } = process.env;
-const mongoose = require('mongoose');
-mongoose.connect(MONGO_URI, {useMongoClient: true});
-
-//MONGOOSE SETUP
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log(`connected to mongoose at ${MONGO_URI}`);
-});
+var db = require('./database.js');
 
 //ROUTES
 app.use(express.static('client'));
 app.use(express.static('node_modules'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
 
-app.post('/movies', (req, res) => {
-    console.log('ROUTE HIT ! gimmie some itunes movies')
-})
+app.get('/shelf', function (req, res) {
+    db.selectAll(function (err, data) {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+app.post('/shelf', function (req, res) {
+    //console.log(req.body);
+    db.add(req.body.params.movie, function (err, res) {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.end("movie on the shelf!");
+        }
+    });
+});
+
+app.delete("/shelf", function (req, res) {
+    console.log(req.body);
+    db.remove(req.body.movieId, function (err) {
+        if (err) {
+            res.sendStatus(404);
+        } else {
+            res.end("that was rotten anyways!");
+        }
+    });
+});
 
 //NOW LISTEN
 app.listen(PORT, (err) => {
