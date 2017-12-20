c6942e02b07424dfb32bcf8002e62249b3919d87
Gareth <gsveitch@gmail.com>
2017-12-20 07:06:37 -0600

    install body-parser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT;
 
 // MONGOOSE REQUIREMENTS
