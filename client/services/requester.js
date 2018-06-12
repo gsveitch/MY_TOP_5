c5487b6112589ee0ec4dfae2915df519f04975bf
Jelani <jhankins02@gmail.com>
2018-06-12 15:29:03 -0500

    (create) movie db service

--- client/services/requester.js
+++ client/services/requester.js
@@ -1,8 +1,23 @@
+const { MOVIEDB } = process.env
 angular.module('movie-shelf')
   .service('itunes', function ($http) {
     this.search = function (query, callback) {
         $http
         .get('https://itunes.apple.com/search?country=us&entity=movie&attribute=featureFilmTerm&limit=10&lang=en_us&term=' + query)
+        .then(function (response) {
+          if (callback) {
+            callback(response.data);
+          }
+        })
+        .catch(function (err) {
+          console.error(err);
+        });
+    };
+  })
+  .service('TheMovieDB', function ($http) {
+    this.search = function (query, callback) {
+      $http
+        .get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIEDB}&query=` + query)
         .then(function (response) {
           if (callback) {
             callback(response.data);
