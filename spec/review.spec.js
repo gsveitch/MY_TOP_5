02ffa8c387aed59874cce94694b4a937e67c8e09
Cain <cainwatson@users.noreply.github.com>
2018-06-12 16:55:57 -0500

    (test) review post test to reject if user already posted one

--- spec/review.spec.js
+++ spec/review.spec.js
@@ -8,39 +8,58 @@ const { PORT } = process.env;
 const endpoint = `http://localhost:${PORT}/review`;
 
 describe('review', () => {
-  let movieId = 4343;
-  let movie;
+  describe('on post', () => {
 
-  beforeEach((done) => {
-    new Movie({ movieId })
-      .save()
-      .then(() => done());
-  });
+    let movieId = 4343;
+    let movie;
 
-  afterEach((done) => {
-    Movie.findOneAndRemove({ movieId })
-      .exec()
-      .then(() => done())
-  });
+    beforeEach((done) => {
+      new Movie({ movieId })
+        .save()
+        .then(() => done());
+    });
 
-  it('post to /review creates a review', (done) => {
-    const review = { message: 'This movie sucked!', userId: 3434 };
+    afterEach((done) => {
+      Movie.findOneAndRemove({ movieId })
+        .exec()
+        .then(() => done())
+    });
 
-    axios.post(endpoint, { movieId, ...review })
-      .then((response) => {
-        expect(response.status).to.equal(200);
-        expect(response.data.error).to.be.null;
-        expect(response.data.data).to.exist;
-      })
-      .then(() => Movie.findOne({ movieId }))
-      .then((updatedMovie) => {
-        const newReview = updatedMovie.reviews[0];
+    it('creates a review', (done) => {
+      const review = { message: 'This movie sucked!', userId: 3434 };
 
-        expect(newReview).to.exist;
-        expect(newReview.message).to.equal(review.message);
-        expect(newReview.userId).to.equal(review.userId);
+      axios.post(endpoint, { movieId, ...review })
+        .then((response) => {
+          expect(response.status).to.equal(200);
+          expect(response.data.error).to.be.null;
+          expect(response.data.data).to.exist;
+        })
+        .then(() => Movie.findOne({ movieId }))
+        .then((updatedMovie) => {
+          const newReview = updatedMovie.reviews[0];
 
-        done();
-      });
-  });
+          expect(newReview).to.exist;
+          expect(newReview.message).to.equal(review.message);
+          expect(newReview.userId).to.equal(review.userId);
+
+          done();
+        });
+    });
+
+    it('should not create a review when already posted one', (done) => {
+      const userId = 3434;
+      const review1 = { message: 'This movie sucked!', userId };
+      const review2 = { message: 'I changed my mind...it\'s not that bad', userId };
+
+      axios.post(endpoint, { movieId, ...review1 })
+        .then(() => axios.post(endpoint, { movieId, ...review2 }))
+        .catch((error) => {
+          expect(error).to.exist;
+          expect(error.response.status).to.equal(500);
+          expect(error.response.data.error).to.be.a('string');
+
+          done();
+        });
+    });
+  })
 });
