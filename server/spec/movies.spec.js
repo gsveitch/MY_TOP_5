63c64c20362490fc66668faf5fa928089df31880
Cain <cainwatson@users.noreply.github.com>
2018-06-12 19:30:49 -0500

    (test) on get to movies

--- server/spec/movies.spec.js
+++ server/spec/movies.spec.js
@@ -8,15 +8,40 @@ const { PORT } = process.env;
 const endpoint = `http://localhost:${PORT}/movies`;
 
 describe('movies', () => {
-  describe('on get', (done) => {
+  const movieId = 4545;
+
+  beforeEach((done) => {
+    new Movie({ movieId })
+      .save()
+      .then(() => done());
   });
 
-  describe('on post', (done) => {
+  afterEach((done) => {
+    Movie.findOneAndRemove({ movieId })
+      .exec()
+      .then(() => done());
   });
 
-  describe('on put', (done) => {
+  describe('on get', () => {
+    it('returns all the movies', (done) => {
+      axios.get(endpoint)
+        .then((response) => {
+          expect(response.status).to.equal(200);
+          expect(response.data.error).to.be.null;
+          expect(response.data.data).to.be.an('array');
+          expect(response.data.data[0].movieId).to.equal(movieId);
+
+          done();
+        });
+    });
   });
 
-  describe('on delete', (done) => {
+  describe('on post', () => {
+  });
+
+  describe('on put', () => {
+  });
+
+  describe('on delete', () => {
   });
 });
