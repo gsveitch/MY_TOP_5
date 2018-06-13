032783af1f97dacfb5546d1dd394ca0ec9dad2e6
Cain <cainwatson@users.noreply.github.com>
2018-06-12 19:48:14 -0500

    (refactor) findMovie to reject on null movie and use in addReview

--- server/database/helpers.js
+++ server/database/helpers.js
@@ -1,28 +1,26 @@
 const { Movie } = require('./');
 
 const getAllMovies = () => Movie.find().exec();
-const findMovie = movieId => Movie.findOne({ movieId }).exec();
+const findMovie = movieId => Movie.findOne({ movieId })
+  .exec()
+  .then(movie => movie || Promise.reject(new Error(`No movie found with movieId: ${movieId}`)));
 const createMovie = ({ movieId }) => {
   const newMovie = new Movie({ movieId });
   return newMovie.save();
 };
 const removeMovie = movieId => Movie.findOneAndRemove({ movieId }).exec();
 
-const addReview = (movieId, review) => {
-  return Movie.findOne({ movieId })
-    .exec()
-    .then(movie => movie || Promise.reject(new Error(`No movie found with movieId: ${movieId}`)))
-    .then((movie) => {
-      const userAlreadyReviewed = !!movie.reviews.find(oldReview => oldReview.userId === review.userId);
+const addReview = (movieId, review) => findMovie(movieId)
+  .then((movie) => {
+    const userAlreadyReviewed = !!movie.reviews.find(oldReview => oldReview.userId === review.userId);
 
-      if (userAlreadyReviewed) {
-        return Promise.reject(new Error('User has already reviewed movie'));
-      }
+    if (userAlreadyReviewed) {
+      return Promise.reject(new Error('User has already reviewed movie'));
+    }
 
-      movie.reviews.push(review);
-      return movie.save();
-    });
-};
+    movie.reviews.push(review);
+    return movie.save();
+  });
 
 module.exports.getAllMovies = getAllMovies;
 module.exports.findMovie = findMovie;
