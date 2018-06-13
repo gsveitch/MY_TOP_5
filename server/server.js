fcc99daf3bea7359157a42cc6af2ae4e26ffc6fc
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-13 11:24:49 -0500

    (server) completed merge refactor

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const passport = require('passport');
+const passportSetup = require('./passportSetup');
 
 const db = require('./database/helpers');
 
@@ -14,6 +15,7 @@ app.use(express.static('client'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.urlencoded({ extended: 'true' }));
 app.use(bodyParser.json());
+passportSetup(app);
 
 app.get('/movies', (req, res) => {
   const { movieId } = req.query;
